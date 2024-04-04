f3a3bd402472f4b6a53de94797fc32dbcfa5dbe1
norita <clothespins5@gmail.com>
2024-04-04 20:32:07 +0900

    トップページコンポーネントを作成。ルーティング処理を実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'pa-root',
+  template: `<router-outlet/>`,
 })
-export class AppComponent {
-  title = 'palettea-web';
-}
+export class AppComponent {}
---
+++ src/app/pages/page-routes.ts
@@ -0,0 +1,9 @@
+import {Routes} from "@angular/router";
+import {TopPageComponent} from "./top-page/top-page.component";
+
+export const PageRoutes: Routes = [
+  {
+    path: 'top',
+    component: TopPageComponent,
+  },
+];
