3f9daccd87ad879aaa404ac43d1d801267d5faaf
norita <kyohei.norita@gmail.com>
2024-04-08 18:14:49 +0900

    TopPageを削除

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'top',
-    loadChildren: () => import('./pages/top-page/top-page-routes').then((m) => m.TopPageRoutes)
-  },
-  {
     path: 'color-picker',
     loadChildren: () => import('./pages/color-picker-page/color-picker-page-routes').then((m) => m.ColorPickerPageRoutes)
   }
--- src/app/pages/top-page/top-page-routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from "@angular/router";
-import {TopPageComponent} from "./top-page.component";
-
-export const TopPageRoutes: Routes = [
-  {
-    path: '',
-    component: TopPageComponent,
-  },
-];
